2a314b1542a51caca631f9fd06b1c93c7e1abc08
MB23 <baidya.medha@gmail.com>
2015-12-15 09:16:19 +0000

    Modified heap content to have OpenDSA specific list visualization

--- javavisualizer/frontAndBackendFiles/backendFiles/JsonFilter.py
+++ javavisualizer/frontAndBackendFiles/backendFiles/JsonFilter.py
@@ -81,216 +81,7 @@ def seperateAndFilterTrace(jUnitTestFile, filesPath, peruserFilesPath, studentfi
 
     return entireJSFile
 
-class Event(object):
-
-    def __init__(self):
-        self.trace = ""
-        self.lineNumber = 0
-
-    def set_Event(self, trace):
-        self.trace = trace
-
-    def set_Line(self, lineNumber):
-        self.lineNumber = lineNumber
-
-    def get_Event():
-        return self.trace
-
-    def get_Line():
-        return self.lineNumber
-
-
-class EventManager(object):
-
-    def __init__(self):
-        self.listOfEvents = []
-        self.filteredEvents = []
-
-    def get_Line_Number(self, index):
-        if is_empty(self.listOfEvents) == True:
-            print("list is empty")
-        else:
-            tempEvent = self.listOfEvents[index]
-            return tempEvent.lineNumber
-
-    def set_Event(self, index, event):
-        self.filteredEvents[index] = event
-
-    def get_Event(index):
-        return self.filteredEvents[index]
-
-    def add_Event(self, event):
-        self.listOfEvents.append(event)
-
-    def trace_List(self):
-        myList = []
-        for x in range(0,len(self.filteredEvents)):
-            temp = Event()
-            temp = self.filteredEvents[x]
-            myList.append(temp.trace)
-
-        return myList
-
-    def print_Events(self):
-        if (len(self.filteredEvents) == 0):
-            print("List of events is empty")
-        else:
-            for x in range(0, len(self.filteredEvents)):
-                tempEvent = self.filteredEvents[x]
-
-    def verify_Events(self):
-        currentLine = 0
-        nextLine = 0
-        eventCounter = 0
-        counter = 1
-
-        originalLineNum = self.get_Line_Number(0)
-        currentLine = originalLineNum
-
-        length = len(self.listOfEvents) - 1
-
-        while counter < length+1:
-            nextLine = self.get_Line_Number(counter)
-
-            if (currentLine+1 == nextLine and nextLine > originalLineNum) or (currentLine+2 == nextLine and nextLine > originalLineNum) or (currentLine+3 == nextLine and nextLine > originalLineNum) or (currentLine+4 == nextLine and nextLine > originalLineNum) or (currentLine+5 == nextLine and nextLine > originalLineNum):
-                self.filteredEvents.append(self.listOfEvents[eventCounter])
-                eventCounter = eventCounter + 1
-                currentLine = nextLine
-                counter = counter + 1
-            else:
-                currentLine = nextLine
-                eventCounter = eventCounter + 1
-                counter = counter + 1
-
-
-        self.filteredEvents.append(self.listOfEvents[length])
-
-    def modify_Lines(self, code):
-        lineNumber = 0
-        eventNumber = 0
-
-        #while lineNumber != len(code):
-        while lineNumber != len(self.filteredEvents):
-            modify = Event()
-            tempString = ""
-            tempLine = 0
-            modify = self.filteredEvents[eventNumber]
-            tempString = modify.trace
-            tempLine = modify.lineNumber
-
-            if(code[lineNumber] == "newline"):
-                lineNumber = lineNumber + 1
-            elif(code[lineNumber] == "\\t"):
-                lineNumber = lineNumber + 1
-            else:
-                originalLine = str(tempLine)
-                newLine = str(lineNumber+1)
-                tempString = tempString.replace(originalLine,newLine)
-                modifiedEvent = Event()
-                modifiedEvent.set_Event(tempString)
-                modifiedEvent.set_Line(lineNumber+1)
-                self.filteredEvents[eventNumber] = modifiedEvent
-
-                eventNumber = eventNumber + 1
-                lineNumber = lineNumber + 1
-
-        lastEvent = Event()
-        if 0 <= eventNumber < len(self.filteredEvents):
-            lastEvent = self.filteredEvents[eventNumber]
-        lastEvent = self.filteredEvents[eventNumber-1]
-        tempLine = lastEvent.lineNumber
-        tempString = lastEvent.trace
-
-        oldLine = str(tempLine)
-
-        secondTLEvent = Event()
-        secondTLEvent = self.filteredEvents[eventNumber-1]
-        otherLine = secondTLEvent.lineNumber
-
-        newLine = str(otherLine)
-
-        tempString = tempString.replace(oldLine,newLine)
-
-        modifiedEvent = Event()
-        modifiedEvent.set_Event(tempString)
-        modifiedEvent.set_Line(otherLine)
-        if 0 <= eventNumber < len(self.filteredEvents):
-            self.filteredEvents[eventNumber-1] = modifiedEvent
-
-
-
-def modifySelectedEventList(eventList,userCodeLen):
-    newList = []
-    #for inde x in range(0, userCodeLen)
-    newList.append(eventList[0])
-    newList.append(eventList[0])
-    newList.append(eventList[0])
-    newList.append(eventList[1])
-
-    return newList
-
-
 class TraceAnalyzer(object):
-
-    def __init__(self):
-        self.eventManager = EventManager()
-
-    #     def handleEverything(self, userCode, inTrace):
-    #         filterPoints = self.exe_Point_Finder
-    #         filterPoints(inTrace)
-    #
-    #         filteredEvents = self.eventManager.verify_Events
-    #         filteredEvents()
-    #         modifyLines = self.eventManager.modify_Lines(userCode)
-    #
-    #         raw_events = []
-    #         raw_events = self.eventManager.trace_List()
-    #
-    #         clean_Events = modifyStack(raw_events)
-    #         consistent_events = clean_Events
-    # 		# making the exceutable events consistent with user code text.
-    #         #consistent_events = modifySelectedEventList(clean_Events,4)
-    #
-    # 	#At this point, everything is ready for output. The printToFile string
-    # 	# contains all of the information necessary for the javaScript file.
-    #         # Its about 20 lines down
-    #         orig_stdout = sys.stdout
-    #         f = file('filteredJSON.js', 'w')
-    #         sys.stdout = f
-    #
-    #         first = "var testvisualizerTrace = {\"code\":\""
-    #
-    #         code = ""
-    #
-    #         for x in range (0,len(userCode)):
-    #             if userCode[x] == "newline":
-    #                 code = code + "\\n" + " "
-    #             else:
-    #                 code = code + userCode[x] + "\\n"
-    #         second = "\",\"trace\":["
-    #
-    #         trace = ""
-    #         for y in range(0,len(consistent_events)):
-    #             if y == len(consistent_events)-1:
-    #                 tempString = consistent_events[y]
-    #                 tempString = tempString[:-1]
-    #                 trace = trace + tempString
-    #                 trace = trace + "],\"userlog\":\"Debugger VM maxMemory: 807M \\n \"}"
-    #                 trace = trace + "\n" + "$(document).ready(function() { \n \n \t var testvisualizer = new ExecutionVisualizer('testvisualizerDiv', testvisualizerTrace,{embeddedMode: false, lang: 'java', heightChangeCallback: redrawAllVisualizerArrows}); \n \n \tfunction redrawAllVisualizerArrows() { \n \n \t \t if (testvisualizer) testvisualizer.redrawConnectors(); \n \t } \n \n $(window).resize(redrawAllVisualizerArrows); \n});"
-    #             else:
-    #                 tempString = consistent_events[y]
-    #                 trace = trace + tempString + "\n"
-    #
-    # 	#String that contains everything for the javascript file
-    # 	# Maybe just send string to front end and write to file in
-    # 	# a directory accessible by the OpenDSA frontend?
-    #         printToFile = first + code + second + trace
-    #
-    #         sys.stdout = orig_stdout
-    #         f.close()
-    #
-    #         return printToFile
-
 
     def handleEverything(self, userCode, inTrace):
         # making the exceutable events consistent with user code text.
@@ -316,8 +107,9 @@ class TraceAnalyzer(object):
         second = "\",\"trace\":["
 
         traceWithoutLine = self.find_executable_trace(inTrace)
+        traceWithoutLine = self.clean_repeated_trace(traceWithoutLine)
         traceWithoutLine = self.clean_stack_trace(traceWithoutLine)
-        #traceWithoutLine = self.clean_heap_trace(traceWithoutLine)
+        traceWithoutLine = self.clean_heap_trace(traceWithoutLine)
         index = 1;
         trace = []
         for traceElement in traceWithoutLine:
@@ -342,53 +134,15 @@ class TraceAnalyzer(object):
         f = first + code + second + final_trace
         #printToFile = first + code + second + final_trace
 
-        #sys.stdout = orig_stdout
-        #f.close()	
+
         return f
         #return printToFile
-
-
-
 
     def is_empty(self, any_structure):
         if any_structure:
             return False
         else:
             return True
-
-    def extract_Line_Num(self, string):
-
-        line = string.replace('"'," ")
-        line = line.replace('{'," ")
-        line = line.replace(':'," ")
-        line = line.replace(','," ")
-        line = line.replace('['," ")
-        line = line.replace('('," ")
-        line = line.replace(']'," ")
-        line = line.replace('}'," ")
-        line = line.replace(')'," ")
-
-        line = [int(s) for s in line.split() if s.isdigit()]
-
-        return line[0]
-
-    def verify_Exe_Point(self, on, off, inPoint):
-
-        addExePoint = False
-        exeTrace = Event()
-
-        if on == True and off == False:
-            exeTrace.set_Event(inPoint)
-            exeTrace.set_Line(self.extract_Line_Num(inPoint))
-            self.eventManager.add_Event(exeTrace)
-            addExePoint = True
-
-        elif on == False and off == False:
-            addExePoint = False
-        else:
-            addExePoint = False
-
-        return addExePoint
 
     def index_containing_substring(self, the_list, substring):
         for i, s in enumerate(the_list):
@@ -407,6 +161,7 @@ class TraceAnalyzer(object):
             trace_line = self.find_line_num(traceElement)
             if line_start <= trace_line <= line_end :
                 cleanTraceList.append(traceElement)
+
         return cleanTraceList
 
 
@@ -434,8 +189,24 @@ class TraceAnalyzer(object):
     def set_line_num(self, stringStepLine, i):
         first = stringStepLine.split('"line":')
         end = first[1].split('"stack_to_render"')
-        stringStepLineReturned = splittedStepLine + '"line":' + i + ',"stack_to_render"' + end
+        stringStepLineReturned = first + '"line":' + i + ',"stack_to_render"' + end
         return stringStepLine
+
+    def clean_repeated_trace(self, clean_trace):
+        prevHeapContent = ''
+        prevStkContent = ''
+
+        for trace in clean_trace:
+            currHeap = self.get_heap_content(trace)
+            currStack = self.get_stack_content(trace)
+
+            if currHeap==prevHeapContent and prevStkContent == currStack:
+                clean_trace.remove(trace)
+
+            prevStkContent = currStack
+            prevHeapContent = currHeap
+
+        return clean_trace
 
     def find_executable_trace(self, trace):
         traceOne = trace.split("stdout")
@@ -451,15 +222,16 @@ class TraceAnalyzer(object):
         indexStartTrace = self.last_index_startTrace(traceStepList, stringStartTrace)
 
         stringEndTrace = 'endTraceNow'
+
         indexEndTrace = self.index_containing_substring(traceStepList, stringEndTrace)
 
-        traceStepList = traceStepList[indexStartTrace+1: indexEndTrace-1]
+
+        traceStepList = traceStepList[indexStartTrace+1: indexEndTrace]
 
         line_Start = self.find_line_num(traceStepList[0])
         line_End = self.find_line_num(traceStepList[len(traceStepList)-1])
 
         clean_trace = self.clean_trace_based_on_num(traceStepList, line_Start, line_End)
-
         return clean_trace
 
     def clean_stack_trace(self, clean_trace):
@@ -479,110 +251,83 @@ class TraceAnalyzer(object):
         clean_trace.pop(0)
         return clean_trace
 
+    def get_heap_content(self, one_trace_instance):
+        heap_str = '"heap":{'
+        heap_end = '}'
+        curHeap = self.find_between(one_trace_instance, heap_str, heap_end)
+        return curHeap
+
+    def get_stack_content(self, one_trace_instance):
+        stack_str = '"encoded_locals":'
+        stack_end = "},"
+        stack = self.find_between(one_trace_instance, stack_str, stack_end)
+        return stack
+
     def clean_heap_trace(self, clean_trace):
-        heap_str = '"heap":'
+        heap_str = '"heap":{'
         heap_end = '}'
         strSplitter = ',["e",'
-        cleanheapTrace = ''
+        traceEndRemainsStr = "}},"
         new_trace = []
         heap_objects = 4
-
         for curr_trace in clean_trace:
-            curHeap = self.find_between(curr_trace, heap_str, heap_end)
-            traceTopRemains = curr_trace.split('"heap')
+             curHeap = self.get_heap_content(curr_trace)
+             traceTopRemains = curr_trace.split('"heap":{')
+             traceEndRemains = traceTopRemains[1].split("}},")
 
-            if curHeap == '' or curHeap == ' ' or curHeap == '{':
-                continue
+             if curHeap == '' or curHeap == ' ' or curHeap == '{':
+                 new_trace.append(str(traceTopRemains[0]) + '"heap": {'+traceEndRemainsStr)
+                 continue
 
-            heapSplit = curHeap.split(strSplitter)
-            cleanedHeap = ''
-            prevHeap = ''
-            totalHeap = []
+             heapSplit = curHeap.split(strSplitter)
+             cleanedHeap = ''
+             totalHeap = []
+             heapPart1 = []
+             numLst = []
+             heapPart2 = []
 
-            idex = 0;
+             idex = 0;
+             for heap in heapSplit:
+                 idex += 1
+                 print heap
+                 num = ''
+                 print num
+                 print idex
+                 if idex != 1:
+                    num = heap[0:1]
+                    num = '["e",'+ num+'],'
+                    numLst.append(num)
+                    heap = heap[4:len(heap)]
+                 else:
+                    heap = heap[0:len(heap)]
 
-            for heap in heapSplit:
+                 print numLst
 
-                #totalHeap = totalHeap + prevHeap
-                if idex == 0:
-                    idex = idex + 1
-                    totalHeap.append(heap)
-                    continue
-                # elif index > heap_objects:
-                # 		continue
+                 heapPart = heap.split('"Link",')
 
-                num = heap[0:2]
-                num = '["e",'+ num+','
+                 if len(heapPart)>1:
+                     heapPart1.append(heapPart.pop(0))
+                     heapPart2.append(str(heapPart.pop())+'],')
+                 else:
+                     #heapPart2.append((str(heapPart2.pop()).rstrip(','))+'],')
+                     heapPart1.append(str(heapPart.pop(0)))
+                 print numLst, heapPart1, heapPart2
 
-                heap = heap[4:len(heap)]
+             while len(heapPart1) > 0:
+                 if len(heapPart2) > 0:
+                     cleanedHeap = cleanedHeap + ''+heapPart1.pop(0) + '"Link",' + numLst.pop(0) + heapPart2.pop(0)
+                     print numLst, heapPart1, heapPart2
+                     print cleanedHeap
+                 else:
+                     cleanedHeap = cleanedHeap +''+heapPart1.pop(0)
 
-                heapPart = heap.split('"Link",')
+             print cleanedHeap
+             totalHeap.append(cleanedHeap)
+             heapContent = ''.join(totalHeap)
+             new_trace.append(str(traceTopRemains[0]) + '"heap": {'+ heapContent+traceEndRemainsStr)
 
-                print heapPart
-
-                if len(heapPart) > 1 :
-                    cleanheap = heapPart[0] + '"Link",' + num + heapPart[1]
-                else:
-                    cleanheap = heapPart[0]
-
-                totalHeap.append(cleanedHeap)
-                idex = idex + 1
-
-            print totalHeap
-            totalHeap.append('}')
-            new_trace.append(traceTopRemains[0] + '"heap": '+ totalHeap)
         return new_trace
 
-    def exe_Point_Finder(self, trace): #This is really messy. Sorry. I will definitely need to refactor in the future
-        symbolStack = []
-        exePointList = []
-        otherList = []
-        currentSymbol = ''
-        topSymbol = ''
-        exe = ""
-        exePoint = " "
-        on = False
-        off = False
-        counter = 0
-
-        for i in trace:
-            currentSymbol = i
-            exePoint = exePoint + currentSymbol
-            if i == '{' or i == '[' or i == '(':
-                symbolStack.append(i)
-            elif i == '}' or i == ')' or i == ']':
-                if self.is_empty(symbolStack) == False:
-                    topSymbol = symbolStack.pop()
-                    if i == '}' and topSymbol != '{':
-                        continue
-            elif i == ',':
-                otherList.append(exePoint)
-                if len(symbolStack) == 0:
-                    for thing in otherList:
-                        exe = exe + thing
-                    if "startTraceNow" in exe:
-                        on = True
-                        exe = ""
-                        exePoint = ""
-                        otherList = []
-                    elif "endTraceNow" in exe:
-                        off = True
-                        return
-                    else:
-                        flag = self.verify_Exe_Point(on, off, exe)
-                        if flag == True:
-                            exe = ""
-                            exePoint = ""
-                            otherList = []
-                        else:
-                            on = False
-                            exe = ""
-                            exePoint = ""
-                            otherList = []
-                else:
-                    exePoint = ""
-            else:
-                continue
 
 def is_empty(structure):
     if structure:
